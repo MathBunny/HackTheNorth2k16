d441cb15500832d66d65dc29e2ae66abebbe1e2b
Apaar Madan <Apaar Madan>
2016-09-17 16:15:14 -0400

    Getting location data

---
+++ PebblePolo/app/src/main/java/com/pebblepolo/pebblepolo/LocationData.java
@@ -0,0 +1,14 @@
+package com.pebblepolo.pebblepolo;
+
+import android.location.Location;
+
+/**
+ * Created by Apaar on 2016-09-17.
+ */
+public class LocationData {
+	public static Location destination;
+	public static Location current;
+
+	public static final long REFRESH_TIME = 3;
+	public static final float REFRESH_DISTANCE = 5;
+}
--- PebblePolo/app/src/main/java/com/pebblepolo/pebblepolo/MainActivity.java
+++ PebblePolo/app/src/main/java/com/pebblepolo/pebblepolo/MainActivity.java
@@ -1,19 +1,56 @@
 package com.pebblepolo.pebblepolo;
 
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import android.location.LocationListener;
+
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+	    LocationListener locationListener = new LocationListener() {
+		    @Override
+		    public void onStatusChanged(String provider, int status, Bundle extras) {
+		    }
+
+		    @Override
+		    public void onProviderEnabled(String provider) {
+		    }
+
+		    @Override
+		    public void onProviderDisabled(String provider) {
+		    }
+
+		    @Override
+		    public void onLocationChanged(Location location) {
+			    if (LocationData.destination != null) {
+				    LocationData.current = location;
+			    }
+		    }
+	    };
+
+	    LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+	    try {
+		    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LocationData.REFRESH_TIME, LocationData.REFRESH_DISTANCE, locationListener);
+	    } catch(SecurityException e) {
+		    Log.e("Error", Log.getStackTraceString(e));
+	    }
     }
 
     public void selectDestination(View v) {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
+
     }
 }
