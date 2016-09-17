97720b15af092e2bbc6bcfb337efe3b823bf9af4
Apaar Madan <Apaar Madan>
2016-09-17 14:40:33 -0400

    Maps activity

--- PebblePolo/app/src/main/java/com/pebblepolo/pebblepolo/MainActivity.java
+++ PebblePolo/app/src/main/java/com/pebblepolo/pebblepolo/MainActivity.java
@@ -1,18 +1,22 @@
 package com.pebblepolo.pebblepolo;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Vibrator v = (Vibrator)getSystemService(VIBRATOR_SERVICE);
-
-        long[] pattern = new long[] {100, 100};
-        v.vibrate(pattern, 0);
+    public void selectDestination(View v) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
