903c59792ea8609095e2c618833bac2de518858f
MathBunny <horatiulazu@gmail.com>
2016-09-18 08:33:31 -0400

    Minor code cleanup

--- PebblePolo/app/src/main/java/com/pebblepolo/pebblepolo/MapsActivity.java
+++ PebblePolo/app/src/main/java/com/pebblepolo/pebblepolo/MapsActivity.java
@@ -1,14 +1,10 @@
 package com.pebblepolo.pebblepolo;
 
-import android.content.Intent;
-import android.location.Geocoder;
-import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
